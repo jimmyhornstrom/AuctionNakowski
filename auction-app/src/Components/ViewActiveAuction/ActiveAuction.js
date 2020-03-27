926d6f058a0f9405f2eb722cd66b6f75ae7dc104
54977238 <mellelin@hotmail.com>
2020-03-27 13:28:48 +0100

    context tester

--- auction-app/src/Components/ViewActiveAuction/ActiveAuction.js
+++ auction-app/src/Components/ViewActiveAuction/ActiveAuction.js
@@ -1,12 +1,18 @@
 import React, {useEffect} from 'react';
 import Navbar from '../Navbar';
+import AuctionInfo from './AuctionInfo';
+import BidInfo from './BidInfo';
+import BidList from './BidList';
+import MakeBidForm from './MakeBidForm';
 
 export default function ActiveAuction() {
 
     return (
         <React.Fragment>
             <Navbar />
-            
+            <AuctionInfo />
+            <MakeBidForm />
+            <BidList />
         </React.Fragment>
     )
 }
