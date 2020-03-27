7605c88a1fe4d023041ba1a0ff3eee50845e71b7
Klerwin <david.winklr@gmail.com>
2020-03-27 12:34:04 +0100

    pre_final

--- auction-app/src/Context/AuctionContext.js
+++ auction-app/src/Context/AuctionContext.js
@@ -4,13 +4,8 @@ export const AuctionContext = createContext();
 
 const AuctionContextProvider = (props) => {
     const [auctions, setAuctions] = useState([]);
-<<<<<<< HEAD
-    const addAuction = (BudID, Summa, AuktionID, Budgivare) => {
-        setAuctions([...auctions, {BudID, Summa, AuktionID, Budgivare}]);
-=======
     const addAuction = (arrayWithAuctions) => {
-        setAuctions([...bids, ...arrayWithAuctions]);
->>>>>>> 8333a252087ff6e2f86607170f92462dd0137ac8
+        setAuctions([...auctions, ...arrayWithAuctions]);
     }
     return(
         <AuctionContext.Provider value={auctions, addAuction}>
