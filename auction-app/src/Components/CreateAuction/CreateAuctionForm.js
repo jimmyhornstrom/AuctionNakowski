3fbe6c87fb1f3543af266c65061b555cfe293107
jimmyhornstrom <jimmy.hornstrom@gmail.com>
2020-03-27 12:35:29 +0100

    sync

---
+++ auction-app/src/Components/CreateAuction/CreateAuctionForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function CreateAuctionForm(props) {
+    return (
+        <div>
+            <form onSubmit={e => props.save(e)}>
+                <input type="text" placeholder="Titel"></input>
+                <input type="text" placeholder="Beskrivning"></input>
+                <input type="text" placeholder="Startdatum"></input>
+                <input type="text" placeholder="Slutdatum"></input>
+                <input type="text" placeholder="Gruppkod"></input>
+                <input type="text" placeholder="Utropspris"></input>
+                <input type="text" placeholder="Skapad av"></input>
+
+            </form>
+        </div>
+    )
+}
