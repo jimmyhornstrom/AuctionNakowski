3a279bc10177c5cb6d603864112bc75f4ebbd0ff
Marcus Sandin <marcussandin@gmail.com>
2020-03-30 12:26:02 +0200

    begining

--- auction-app/src/App.js
+++ auction-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Test from './Components/CreateAuction/test'
+// import Test from './Components/CreateAuction/test'
 import BidContextProvider from './Context/BidContext';
 import ActiveAuction from './Components/ViewActiveAuction/ActiveAuction';
 import AuctionContextProvider from './Context/AuctionContext';
+import Main from './Components/Main';
+import Navbar from './Components/Navbar'
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
   return (
     <AuctionContextProvider>
       <BidContextProvider>
+        <Navbar/>
+        <Main/>
           {/* <Test /> */}
           <ActiveAuction />
       </BidContextProvider>
---
+++ auction-app/src/Components/Navbar.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+
+    return(
+        <React.Fragment>
+        <nav>
+            <ul>
+                <li><NavLink to="./ViewActiveAuction/ActiveAuction">Hem</NavLink></li>
+                <li><NavLink to='./CreateAuction/CreateAuctionForm'>Skapa ny auktion</NavLink></li>
+                <li><NavLink to="./ViewOldAuction/OldAuction">Avslutade auktioner</NavLink></li>
+            </ul>            
+        </nav>      
+        </React.Fragment>
+    );
+
+};
+
+export default Navbar;
