21d0d3bd90cd572e526112d4648b38e34ef3f992
Klerwin <david.winklr@gmail.com>
2020-03-30 11:38:30 +0200

    app fix

--- auction-app/src/App.js
+++ auction-app/src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import './App.css';
 import Test from './Components/CreateAuction/test'
 import BidContextProvider from './Context/BidContext';
-<<<<<<< HEAD
-import StartView from './Components/StartView/StartView'
-=======
 import ActiveAuction from './Components/ViewActiveAuction/ActiveAuction';
 import AuctionContextProvider from './Context/AuctionContext';
->>>>>>> 3b15be75e69f65878934a2cc4830ba15befd3bc5
 
 
 function App() {
