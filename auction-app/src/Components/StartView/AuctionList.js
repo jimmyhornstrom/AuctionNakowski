ba3f082f07a6fc832beae2e56a9dc95c0ff9b2e9
Klerwin <david.winklr@gmail.com>
2020-03-31 20:46:41 +0200

    sök

--- auction-app/src/Components/StartView/AuctionList.js
+++ auction-app/src/Components/StartView/AuctionList.js
@@ -3,33 +3,18 @@ import classes from "./StartView.module.css";
 import { AuctionContext } from '../../Context/AuctionContext';
 import AuctionItem from './AuctionDetail';
 
-
-
 const AuctionList = () =>{
 
 let[auctionId, setAuctionId ] = useState();
 let[error, setError] = useState(false);
 const {auctions} = useContext(AuctionContext)
-
-
-
-function getList(e){
-
-}   
-
-    let list =  auctions.map(a => { console.log(a + "hej")
+  
+    let list =  auctions.map(a => { 
         return (<AuctionItem auction={a} />)})
-   
 return(
-
     <div>
         {list}
-        <p>{JSON.stringify(auctions)}hej hej</p>
-        
-       
     </div>
-)
-
-}
+)}
 
 export default AuctionList;
