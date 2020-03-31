0e0fb88af18f8e24500d8255b5087285c0fb8f1d
54977238 <mellelin@hotmail.com>
2020-03-31 14:40:23 +0200

    .

--- auction-app/src/App.js
+++ auction-app/src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Navbar/>
         <Main/>
         {/* <StartView/> */}
-        <CreateAuctionForm />
+        {/* <CreateAuctionForm /> */}
         <ActiveAuction />
       </BidContextProvider>
     </AuctionContextProvider>
