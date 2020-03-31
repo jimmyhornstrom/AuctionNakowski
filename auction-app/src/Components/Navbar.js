622c7770b65d619efceab99cbe084723a6bde386
54977238 <mellelin@hotmail.com>
2020-03-31 14:33:10 +0200

    home knappen funkar

--- auction-app/src/Components/Navbar.js
+++ auction-app/src/Components/Navbar.js
@@ -3,12 +3,12 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
-
+//ViewActiveAuction/ActiveAuction
     return(
         <React.Fragment>
         <nav>
             <ul className="navbarStyle">
-                <li><NavLink to="/ViewActiveAuction/ActiveAuction">Hem</NavLink></li>
+                <li><NavLink to="/">Hem</NavLink></li>
                 <li><NavLink to='/CreateAuction/CreateAuctionForm'>Skapa ny auktion</NavLink></li>
                 {/* <li><NavLink to="/ViewOldAuction/OldAuction">Avslutade auktioner</NavLink></li> */}
             </ul>            
