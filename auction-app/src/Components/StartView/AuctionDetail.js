c1e597ac2eb4c994b9d0e6ea44314956fe77d356
54977238 <mellelin@hotmail.com>
2020-04-01 13:43:18 +0200

    datum display

--- auction-app/src/Components/StartView/AuctionDetail.js
+++ auction-app/src/Components/StartView/AuctionDetail.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 //Skapar ett auction item
 const AuctionItem = (props) => {
-
+    let displayStart = props.auction.StartDatum.substring(8, 10) + "/" + props.auction.StartDatum.substring(5, 7) + "/" + props.auction.StartDatum.substring(0, 4);
+    let displaySlut = props.auction.SlutDatum.substring(8, 10) + "/" + props.auction.SlutDatum.substring(5, 7) + "/" + props.auction.SlutDatum.substring(0, 4);
     return (<React.Fragment>
          <h3>{props.auction.Titel}</h3>
             <h4>pris: {props.auction.Utropspris} :-</h4>
-            <p><i>från {props.auction.StartDatum} till {props.auction.SlutDatum}</i></p>
+            <p><i>från {displayStart} till {displaySlut}</i></p>
             <h5>Skapad av: {props.auction.SkapadAv}</h5>
     </React.Fragment>)
 }
