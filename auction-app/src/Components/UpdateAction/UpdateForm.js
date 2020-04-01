5154e67d5441aacf2043a702329e13ee549656b3
54977238 <mellelin@hotmail.com>
2020-04-01 10:40:39 +0200

    update kan renderas

--- auction-app/src/Components/UpdateAction/UpdateForm.js
+++ auction-app/src/Components/UpdateAction/UpdateForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AuctionContext } from "../../Context/AuctionContext";
 
 const UpdateForm = props => {
@@ -10,18 +10,24 @@ const UpdateForm = props => {
   const [endDate, setEndDate] = useState(new Date());
   const [startBid, setStartBid] = useState(0);
   const [createdBy, setCreatedBy] = useState("");
+  //const [cA, setCA] = useState({});
 
-  let currentAuction = auctions.find(a => {
-    //test
-    console.log("auktions-id:" + a.AuktionID +" titel:" +a.Titel +".... props-id:" +props.id);
+  //useEffect(() => {
+        let currentAuction = auctions.find(a => {
+        //test
+        console.log("auktions-id:" + a.AuktionID +" titel:" +a.Titel +".... props-id:" +props.id);
 
-    return a.AuktionID === props.id;
-  });
+        return a.AuktionID === props.id;
+      });
+      // test // allt logga flera gånger i consolen??
+      console.log("visas här: " + JSON.stringify(currentAuction)); //funkar: loggar ut som en sträng
+      console.log(currentAuction); //funkar: loggar ut objektet
+      
+      //setCA(currentAuction);
+// },[])
+  
 
-  // test // allt logga flera gånger i consolen??
-  console.log("visas här: " + JSON.stringify(currentAuction)); //funkar: loggar ut som en sträng
-  console.log(currentAuction); //funkar: loggar ut objektet
-  //
+  
 
   const SetNewAuctionValues = e => {
     e.preventDefault();
@@ -40,24 +46,22 @@ const UpdateForm = props => {
     updateAuction(auctionWithNewValues);
   };
 
-  return (
-    <h1>test</h1>
-
-    //     <form onSubmit={SetNewAuctionValues}>
-    //     <label>Titel</label>
-    //     <input type="text" placeholder={JSON.stringify(currentAuction.Titel)} onChange={(e) => setTitel(e.target.value)}></input>
-    //     <label>Beskrivning</label>
-    //     <input type="text" placeholder={currentAuction.Beskrivning} onChange={(e) => setDescription(e.target.value)}></input>
-    //     <label>StartDatum</label>
-    //     <input type="datetime-local" placeholder={currentAuction.StartDatum} onChange={(e) => setStartDate(e.target.value)}></input>
-    //     <label>SlutDatum</label>
-    //     <input type="datetime-local" placeholder={currentAuction.SlutDatum} onChange={(e) => setEndDate(e.target.value)}></input>
-    //     <label>Utropspris</label>
-    //     <input type="number" placeholder={currentAuction.Utropspris} onChange={(e) => setStartBid(e.target.value)}></input>
-    //     <label>Skapad av</label>
-    //     <input type="text" placeholder={currentAuction.SkapadAv} onChange={(e) => setCreatedBy(e.target.value)}></input>
-    //     <button type="submit">Spara</button>
-    // </form>
-  );
+  return auctions.length ? (
+      <form onSubmit={SetNewAuctionValues}><h1>test</h1>
+        <label>Titel</label>
+        <input type="text" placeholder={JSON.stringify(currentAuction.Titel)} onChange={(e) => setTitel(e.target.value)}></input>
+        <label>Beskrivning</label>
+        <input type="text" placeholder={currentAuction.Beskrivning} onChange={(e) => setDescription(e.target.value)}></input>
+        <label>StartDatum</label>
+        <input type="datetime-local" placeholder={currentAuction.StartDatum} onChange={(e) => setStartDate(e.target.value)}></input>
+        <label>SlutDatum</label>
+        <input type="datetime-local" placeholder={currentAuction.SlutDatum} onChange={(e) => setEndDate(e.target.value)}></input>
+        <label>Utropspris</label>
+        <input type="number" placeholder={currentAuction.Utropspris} onChange={(e) => setStartBid(e.target.value)}></input>
+        <label>Skapad av</label>
+        <input type="text" placeholder={currentAuction.SkapadAv} onChange={(e) => setCreatedBy(e.target.value)}></input>
+        <button type="submit">Spara</button>
+    </form>
+  ) : (<p>loading...</p>)
 };
 export default UpdateForm;
